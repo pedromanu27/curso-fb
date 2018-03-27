94549890f86aa9c693351eea2b317efca0a1edf7
pedromanu27 <pedromanu27@gmail.com>
2018-03-26 23:56:16 -0300

    Primeiro commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCbtl_c8XYwwGC-Uh_fqM2H9BhQ4JPBd80",
+    authDomain: "curso-fb-f56df.firebaseapp.com",
+    databaseURL: "https://curso-fb-f56df.firebaseio.com",
+    projectId: "curso-fb-f56df",
+    storageBucket: "curso-fb-f56df.appspot.com",
+    messagingSenderId: "781958053876"
+};
+firebase.initializeApp(config);
